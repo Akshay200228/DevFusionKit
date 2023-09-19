27947cad6c3ad7f775f6dfefb6ede0e6f6199537
Akshay200228 <akshayrs096@gmail.com>
2023-09-19 17:33:29 +0530

    Add CodeBlock section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Account from "@/components/Account";
 import CallToAction from "@/components/CallToAction";
+import CodeEditor from "@/components/CodeEditor";
 import Feature from "@/components/Feature";
 import Hero from "@/components/Hero";
 import Reviews from "@/components/Reviews";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <CodeEditor />
       <Feature />
       <Reviews />
       <Account />
--- src/images/index.js
+++ src/images/index.js
@@ -1,9 +1,5 @@
-import herogif from "./herogif.gif"
-import heromeet from "./heromeet.gif"
 import dboy from "./dboy.png"
 
 export {
-    herogif,
-    heromeet,
     dboy,
 }
