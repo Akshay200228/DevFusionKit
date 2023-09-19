fc008e26eea24edbe99ca626d59103a6189bb87f
Akshay200228 <akshayrs096@gmail.com>
2023-09-19 17:12:23 +0530

    Improve logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,20 +6,24 @@ const Logo = ({ className, props }) => {
     <Link href={"/"}>
       <h2
         className={clsx(
-          "text-3xl font-bold text-blue-500 duration-300" ,
+          "text-3xl font-bold text-blue-500 duration-300",
           className
         )}
         {...props}
       >
         Dev
-        <span className="text-[#F5F5F5]" style={{ textShadow: "1px 1px 2px #000" }}>
-          Fusion
-        </span>
+        <span className="text-stroke">Fusion</span>
         Kit
       </h2>
       <style jsx>{`
         .text-[#6e8157] {
-          color: #6e8157; /* Define the green color here */
+          color: #6e8157;
+        }
+
+        .text-stroke {
+          -webkit-text-stroke: 1px #2196F3;
+          text-stroke: 1px #000;
+          color: #F5F5F5;
         }
       `}</style>
     </Link>
