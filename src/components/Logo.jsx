68c5d0bc11b99dda07350cb95cdd3417d2b6b606
sahilwagh03 <sahilwagh142@gmail.com>
2023-09-22 11:25:32 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,22 +1,30 @@
 import Link from "next/link";
 import clsx from "clsx";
+import Image from "next/image";
+import { devLogo } from "@/images";
 
 const Logo = ({ className, props }) => {
   return (
     <Link href={"/"}>
-      <h2
-        className={clsx(
-          "text-3xl font-bold text-blue-500 duration-300",
-          className
-        )}
-        {...props}
-      >
-        DevFusionKit
-        {/* Dev
+      <div className="flex justify-center items-center">
+        <Image
+          src={devLogo}
+          alt="Logo"
+          className="w-[4rem] h-auto"
+        />
+        <h2
+          className={clsx(
+            "text-3xl font-bold text-blue-500 duration-300",
+            className
+          )}
+          {...props}
+        >
+          DevFusionKit
+          {/* Dev
         <span className="text-stroke">Fusion</span>
         Kit */}
-      </h2>
-      <style jsx>{`
+        </h2>
+        <style jsx>{`
         .text-[#6e8157] {
           color: #6e8157;
         }
@@ -27,6 +35,7 @@ const Logo = ({ className, props }) => {
           color: #F5F5F5;
         }
       `}</style>
+      </div>
     </Link>
   );
 };
