1b9e8c99562a038a85f7a2ff21ce34d92c14838e
Akshay200228 <akshayrs096@gmail.com>
2023-09-22 13:36:45 +0530

    Modified

--- src/app/component/page.jsx
+++ src/app/component/page.jsx
@@ -1,23 +1,12 @@
-import Sidebar from '@/components/comLayout/Sidebar';
-import SearchBar from '@/components/comLayout/SearchBar';
 import Card from '@/components/comLayout/card';
 
 export default function Component() {
 
   return (
-    <div className="flex h-[100vh]">
-      {/* Responsive SideBar */}
-      <Sidebar />
-
-      {/* Right Showcase Work */}
-      <div className="w-full bg-gray-100">
-
-        {/* Search Input */}
-        <SearchBar />
-
-        {/* Scrollable Cards */}
-        <Card />
-      </div>
+    // Work Showcase
+    <div className="w-full bg-gray-100">
+      {/* Scrollable Cards */}
+      <Card />
     </div>
   );
 }
