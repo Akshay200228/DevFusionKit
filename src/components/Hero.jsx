10d206b5dedc9a45fc3bfe809237849e866626f0
Akshay200228 <akshayrs096@gmail.com>
2023-09-24 11:16:31 +0530

    improve hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="h-auto py-10 overflow-hidden mb-28 xl:h-screen md:mb-0 sm:py-20 lg:pb-32 xl:pb-36"
+      className="h-auto py-10 overflow-hidden mb-28 md:mb-0 sm:py-20 lg:pb-32 xl:pb-36"
     >
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-28">
