268f449d21a9b3b82ecadda7ad22def6f5d9f42c
Akshay200228 <akshayrs096@gmail.com>
2023-09-24 10:53:46 +0530

    improve skelton loading

--- src/components/comLayout/Card.jsx
+++ src/components/comLayout/Card.jsx
@@ -61,7 +61,7 @@ export default function CardComponent() {
                                     width={400}
                                     height={300}
                                     priority
-                                    className="object-fill w-full mb-2 rounded-lg cursor-pointer h-60"
+                                    className="object-fill w-full mb-4 rounded-lg cursor-pointer h-60"
                                 />
                                 <h2 className="mb-2 text-xl font-semibold text-gray-600">{card.title}</h2>
                                 <p className="text-gray-600">{card.content}</p>
--- src/components/comLayout/CardSkeleton.jsx
+++ src/components/comLayout/CardSkeleton.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const CardSkeleton = () => {
     return (
-        <div className="p-4 bg-white rounded-lg shadow-md animate-pulse">
-            <div className="w-full h-40 mb-2 bg-gray-300 rounded-lg"></div>
+        <div className="p-4 bg-white rounded-lg shadow-md h-96 animate-pulse">
+            <div className="w-full mb-2 bg-gray-300 rounded-lg h-60"></div>
             <div className="h-6 mb-2 bg-gray-300 rounded"></div>
             <div className="h-6 bg-gray-300 rounded"></div>
         </div>
