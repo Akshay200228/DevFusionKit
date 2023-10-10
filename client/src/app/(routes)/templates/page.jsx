70df115e33dbe513fd5342ff1f5d27926def81f0
Akshay200228 <akshayrs096@gmail.com>
2023-10-10 12:28:19 +0530

    add

--- client/src/app/(routes)/templates/page.jsx
+++ client/src/app/(routes)/templates/page.jsx
@@ -12,7 +12,7 @@ export default function Templates() {
       <div className="max-w-4xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8">
         <div className="text-center">
           <h1 className="text-3xl font-extrabold xl:leading-[62px] md:leading-[52px] leading-[42px] text-gray-900 sm:text-4xl lg:text-5xl">
-            Provide Templates for Websites by{" "}
+            Provide Templates for Websites by,{" "}
             <span className="relative inline-block">
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 animate-stroke">
                 DevNexus
