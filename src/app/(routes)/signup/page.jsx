ce4b738e4a94accb586a7105fd5246eb04cfa7fa
Akshay200228 <akshayrs096@gmail.com>
2023-10-17 11:46:21 +0530

    new link

--- src/app/(routes)/signup/page.jsx
+++ src/app/(routes)/signup/page.jsx
@@ -27,9 +27,9 @@ export default function SignUp() {
     e.preventDefault();
     try {
       // const response = await axios.post('http://localhost:8000/api/users/signup', formData);
-      // const apiUrl = process.env.NEXT_PUBLIC || 'https://devnexus-server.onrender.com';
+      const apiUrl = process.env.NEXT_PUBLIC || 'https://devnexus-server.onrender.com';
 
-      const response = await axios.post(`${process.env.NEXT_PUBLIC}/api/users/signup`, formData);
+      const response = await axios.post(`${apiUrl}/api/users/signup`, formData);
       console.log(response.data);
       setSuccessMessage('User registered successfully!');
       router.push('/login');
