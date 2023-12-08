8f7a7172810d2a9717fc9fac8d9ba048ad294aff
Akshay200228 <akshayrs096@gmail.com>
2023-12-08 13:17:18 +0530

    try to solve routing issue

---
+++ src/app/(routes)/component/CodeComponentDetail/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+// CodeComponentDetail.jsx
+import { LivePreview, LiveProvider } from 'react-live';
+import { devLogo } from '@/images';
+import useApiFetch from '@/hooks/useApiFetch';
+
+export default function CodeComponentDetail({ slug }) {
+    console.log("Slug ID: ", slug);
+    const apiUrl = `https://devnexus-server.onrender.com/api/code-components/${slug}`;
+    const { data: codeComponent, isLoading, error } = useApiFetch(apiUrl);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return (
+            <div>
+                Error: {error.message} - This code component may not exist or there was an issue fetching data.
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>{codeComponent.title}</h1>
+            <LiveProvider code={codeComponent.code}>
+                <div className="h-[50vh] mb-4 bg-blue-200 relative overflow-hidden rounded-lg">
+                    <div className="absolute inset-0 text-neutral-950">
+                        <LivePreview />
+                    </div>
+                </div>
+            </LiveProvider>
+            <div className="flex items-center">
+                <img src={devLogo} alt="User Image" width={36} height={36} className="mr-2 rounded-full" />
+                <h2 className="text-xl font-semibold text-gray-600">{codeComponent.title}</h2>
+            </div>
+        </div>
+    );
+}
--- src/components/comLayout/Card.jsx
+++ src/components/comLayout/Card.jsx
@@ -5,22 +5,24 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { LivePreview, LiveProvider } from 'react-live';
 import { devLogo } from '@/images';
-import { useRouter } from 'next/navigation';
 import useApiFetch from '@/hooks/useApiFetch';
 import Loader from '../Loader';
+import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
+
 
 export default function CardComponent() {
-    // const apiUrl = process.env.NEXT_PUBLIC_NEXUS_URL + "/api/code-components/" || "http://localhost:8000/api/code-components/";
     const apiUrl = "https://devnexus-server.onrender.com/api/code-components/";
-    
+
     const { data: cardData, isLoading, error } = useApiFetch(apiUrl);
 
-    const router = useRouter();
+    // const router = useRouter();
 
-    const handleViewMore = (slug) => {
-        console.log("Id: ", slug);
-        router.push(`/code-comp/${slug}`);
-    };
+    // const handleViewMore = (slug) => {
+    //     console.log("Id: ", slug);
+    //     router.push(`/component/CodeComponentDetail/${slug}`);
+    // };
+
 
     return (
         <>
@@ -42,42 +44,48 @@ export default function CardComponent() {
                         transition={{ delay: 0.1, duration: 0.5 }}
                         className="grid grid-cols-1 gap-8 p-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
                     >
-                        {cardData.map((card) => (
-                            <motion.div
-                                key={card._id}
-                                initial={{ opacity: 0, scale: 0.9 }}
-                                animate={{ opacity: 1, scale: 1 }}
-                                transition={{ duration: 0.3 }}
-                                className="flex flex-col h-full p-4 bg-white rounded-lg shadow-xl"
-                            >
-                                <LiveProvider code={card.code}>
-                                    <div className="h-[50vh] mb-4 bg-blue-200 relative overflow-hidden rounded-lg">
-                                        <div className="absolute inset-0 text-neutral-950">
-                                            <LivePreview />
+                        {cardData.map((card) => {
+                            return (
+                                <motion.div
+                                    key={card._id}
+                                    initial={{ opacity: 0, scale: 0.9 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="flex flex-col h-full p-4 bg-white rounded-lg shadow-xl"
+                                >
+                                    <LiveProvider code={card.code}>
+                                        <div className="h-[50vh] mb-4 bg-blue-200 relative overflow-hidden rounded-lg">
+                                            <div className="absolute inset-0 text-neutral-950">
+                                                <LivePreview />
+                                            </div>
                                         </div>
-                                    </div>
-                                </LiveProvider>
+                                    </LiveProvider>
 
-                                <div className="flex items-center justify-between mb-2">
-                                    <div className="flex items-center">
-                                        <Image
-                                            src={devLogo}
-                                            alt="User Image"
-                                            width={36}
-                                            height={36}
-                                            className="mr-2 rounded-full"
-                                        />
-                                        <h2 className="text-xl font-semibold text-gray-600">{card.title}</h2>
+                                    <div className="flex items-center justify-between mb-2">
+                                        <div className="flex items-center">
+                                            <Image
+                                                src={devLogo}
+                                                alt="User Image"
+                                                width={36}
+                                                height={36}
+                                                className="mr-2 rounded-full"
+                                            />
+                                            <h2 className="text-xl font-semibold text-gray-600">{card.title}</h2>
+                                        </div>
+                                        {card._id && (
+                                            <Link href={{ pathname: "/component/CodeComponentDetail", query: { slug: card._id } }}>
+                                                <button
+                                                    className="p-2 text-blue-400 rounded-full hover-bg-blue-200"
+                                                    onClick={() => console.log("Button Slug ID:", card._id)}
+                                                >
+                                                    <FaCode className="text-4xl" />
+                                                </button>
+                                            </Link>
+                                        )}
                                     </div>
-                                    <button
-                                        onClick={() => handleViewMore(card._id)}
-                                        className="p-2 text-blue-400 rounded-full hover-bg-blue-200"
-                                    >
-                                        <FaCode className="text-4xl" />
-                                    </button>
-                                </div>
-                            </motion.div>
-                        ))}
+                                </motion.div>
+                            );
+                        })}
                     </motion.div>
                 )}
             </motion.div>
