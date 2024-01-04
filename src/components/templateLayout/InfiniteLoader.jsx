cfbadd23a3c350ed42fb3b2c9a993575abe8e3cb
Akshay200228 <akshayrs096@gmail.com>
2024-01-04 13:14:59 +0530

    try5

--- src/components/templateLayout/InfiniteLoader.jsx
+++ src/components/templateLayout/InfiniteLoader.jsx
@@ -1,66 +1,88 @@
 "use client";
+
 // InfiniteLoader.jsx
 import { fetchWebTemp } from "@/app/action";
-import { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import { useEffect, useRef, useState } from "react";
 import WebTempCard from "./WebTempCard";
 
-
 export default function InfiniteLoader({ height = 'h-24' }) {
-    const { ref, inView } = useInView();
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
-    const [allDataLoaded, setAllDataLoaded] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [allDataLoaded, setAllDataLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            if (inView && !allDataLoaded && !loading) {
-                try {
-                    setLoading(true); // Set loading to true when fetching
-                    const newData = await fetchWebTemp(page);
-                    if (newData.length > 0) {
-                        setData((prevData) => [...prevData, ...newData]);
-                        setPage((prevPage) => prevPage + 1);
-                    } else {
-                        setAllDataLoaded(true); // Stop loader when all data is loaded
-                    }
-                } catch (error) {
-                    console.error("Error fetching data:", error);
-                } finally {
-                    setLoading(false); // Set loading to false when fetching is complete
-                }
-            }
-        };
+  const loaderRef = useRef(null);
 
-        fetchData();
-    }, [inView, page, allDataLoaded, loading]);
+  const fetchData = async () => {
+    if (!allDataLoaded && !loading) {
+      try {
+        setLoading(true);
+        const newData = await fetchWebTemp(page);
+        if (newData.length > 0) {
+          setData((prevData) => [...prevData, ...newData]);
+          setPage((prevPage) => prevPage + 1);
+        } else {
+          setAllDataLoaded(true);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
 
-    return (
-        <>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-                {data.map((webtemp) => (
-                    <WebTempCard key={webtemp._id} webtemp={webtemp} />
-                ))}
-            </div>
-            {!allDataLoaded && loading && (
-                <div
-                    className={`flex items-center justify-center ${height}`}
-                >
-                    <div className="w-16 h-16 border-t-4 border-b-4 border-red-500 rounded-full animate-spin" />
-                </div>
-            )}
-            {!allDataLoaded && !loading && (
-                <div
-                    ref={ref}
-                    className={`flex items-center justify-center ${height}`}
-                >
-                    <div className="w-16 h-16 border-t-4 border-b-4 border-red-500 rounded-full animate-spin" />
-                </div>
-            )}
-            {allDataLoaded && (
-                <h3 className="mt-4 text-xl text-center">Thank you for viewing!</h3>
-            )}
-        </>
-    );
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20% 0px", // Adjust this value as needed
+      threshold: 0.5,
+    };
+
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          fetchData();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [loaderRef, fetchData]);
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        {data.map((webtemp) => (
+          <WebTempCard key={webtemp._id} webtemp={webtemp} />
+        ))}
+      </div>
+      {!allDataLoaded && loading && (
+        <div className={`flex items-center justify-center ${height}`}>
+          <div className="w-16 h-16 border-t-4 border-b-4 border-red-500 rounded-full animate-spin" />
+        </div>
+      )}
+      {!allDataLoaded && !loading && (
+        <div
+          ref={loaderRef}
+          className={`flex items-center justify-center ${height}`}
+        >
+          <div className="w-16 h-16 border-t-4 border-b-4 border-red-500 rounded-full animate-spin" />
+        </div>
+      )}
+      {allDataLoaded && (
+        <h3 className="mt-4 text-xl text-center">Thank you for viewing!</h3>
+      )}
+    </>
+  );
 }
