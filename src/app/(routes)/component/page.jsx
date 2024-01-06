a8aaee7a167e1a113d4988a58b3375e85d95926f
Akshay200228 <akshayrs096@gmail.com>
2024-01-06 12:31:22 +0530

    Implement search feature

--- src/app/(routes)/component/page.jsx
+++ src/app/(routes)/component/page.jsx
@@ -1,11 +1,8 @@
+"use client"
 import Card from '../../../components/comLayout/Card';
 
-export const metadata = {
-  title: 'Components'
-}
-
 export default function Component() {
-  
+
   return (
     <div className="w-full bg-white">
       <Card />
--- src/components/comLayout/SearchBar.jsx
+++ src/components/comLayout/SearchBar.jsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { useState } from "react";
 import { HiSearch } from 'react-icons/hi';
 import Link from 'next/link';
 import Container from "../homeLayout/Container";
+import { useSearch } from '@/context/SearchContext';
 
 export default function SearchBar() {
-    const [searchQuery, setSearchQuery] = useState('');
+    const { searchQuery, handleSearch } = useSearch();
 
-    const handleSearch = () => {
-        // Handle Search comming soon
+    const handleChange = (e) => {
+        const value = e.target.value;
+        console.log('Input value:', value);
+        handleSearch(value);
     };
 
     return (
@@ -20,7 +22,7 @@ export default function SearchBar() {
                         type="text"
                         placeholder="Search by title"
                         value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onChange={handleChange}
                         className="w-full py-2 pl-10 pr-4 text-gray-700 placeholder-gray-500 placeholder-opacity-75 bg-white border border-gray-300 rounded-lg focus:border-blue-500 focus:ring focus:ring-blue-200 hover:border-blue-600 focus:outline-none"
                     />
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -28,7 +30,7 @@ export default function SearchBar() {
                     </div>
                 </div>
 
-                <Link href="/code-comp" className="px-2 py-2 ml-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+                <Link href="/code-comp" className="px-2 py-2 ml-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
                     + Create
                 </Link>
             </div>
