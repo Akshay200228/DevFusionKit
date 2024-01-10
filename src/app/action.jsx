dc075baddc7d05d0275ea851be53e2984d3ca884
Akshay200228 <akshayrs096@gmail.com>
2024-01-10 20:02:19 +0530

    clean

--- src/app/action.jsx
+++ src/app/action.jsx
@@ -22,15 +22,14 @@ export async function fetchWebTemp(currentPage) {
 }
 
 // Code comp card data
-
-export async function getCodeComp(page) {
-    try {
-        // Fetch api here
-        const response = await fetch(`http://localhost:8000/api/code-components?page=${page}`);
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        console.log("Error is here: ", err);
-        return [];
-    }
-}
+// export async function getCodeComp(page) {
+//     try {
+//         // Fetch api here
+//         const response = await fetch(`http://localhost:8000/api/code-components?page=${page}`);
+//         const data = await response.json();
+//         return data;
+//     } catch (err) {
+//         console.log("Error is here: ", err);
+//         return [];
+//     }
+// }
