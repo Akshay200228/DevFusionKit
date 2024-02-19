a2d302644e1fbf59f9b6071b42afd03fc521ef3b
Akshay200228 <akshayrs096@gmail.com>
2024-02-19 16:11:02 +0530

    Add skelton loading for spotlight

--- src/app/(routes)/spotlight/page.jsx
+++ src/app/(routes)/spotlight/page.jsx
@@ -1,16 +1,15 @@
 'use client';
 import Link from 'next/link';
-import UserAvatar from "@/components/UserAvatar";
 import Container from "@/components/homeLayout/Container";
 import useApiFetch from "@/hooks/useApiFetch";
 import { FaCode } from "react-icons/fa";
 import { LivePreview, LiveProvider } from 'react-live';
-import { CardSkeleton } from '@/components/SkeltonLoading';
+import { SpotlightSkeleton } from '@/components/SkeltonLoading';
 
 const Spotlight = () => {
     // Define the API URL
-    const apiUrl = process.env.NEXT_PUBLIC_NEXUS_URL+"/api/code-components";
-    
+    const apiUrl = process.env.NEXT_PUBLIC_NEXUS_URL + "/api/code-components";
+
     // Call the useApiFetch hook with the API URL
     const { data: cardData, isLoading: cardLoading, error: cardError } = useApiFetch(apiUrl);
 
@@ -19,52 +18,48 @@ const Spotlight = () => {
         // Sort code components based on the number of bookmarks (descending order)
         const sortedData = data.sort((a, b) => b.bookmarks.length - a.bookmarks.length);
         // Slice the top 6 code components
-        return sortedData.slice(0, 6);
+        return sortedData.slice(0, 8);
     };
 
     // Get top 6 code components with the largest number of bookmarks
     const topBookmarkedCodeComps = cardLoading ? [] : getTopBookmarkedCodeComps(cardData);
 
-    if (cardLoading) {
-        return (
-            <CardSkeleton count={9} />
-        );
-    }
     return (
         <Container>
-            <div className="min-h-screen pt-4">
-                <h1 className='text-2xl font-bold text-center text-slate-800'>
+            <div className="min-h-screen px-4 py-8">
+                <h1 className="mb-4 text-4xl font-bold text-center text-slate-800">
                     Top Posts
                 </h1>
-                <div className="grid grid-cols-1 gap-8 p-4 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-                    {topBookmarkedCodeComps.map((card) => (
-                        <div key={card._id} className="relative flex flex-col h-auto bg-white rounded-lg shadow-xl transform-style-preserve-3d hover:shadow-2xl">
-                            <LiveProvider code={card.code}>
-                                <div className="min-h-[50vh] mb-4 bg-gradient-to-r from-blue-300 to-blue-200 relative rounded-t-lg transform-style-preserve-3d">
-                                    <div className="absolute inset-0 overflow-auto text-neutral-950 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-blue-200">
-                                        <LivePreview />
+                <p className="mb-8 text-lg font-bold text-center text-blue-500 text-palanquin">
+                    All time
+                </p>
+                {cardLoading ? (
+                    <SpotlightSkeleton count={8} />
+                ) : (
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {topBookmarkedCodeComps.map((card) => (
+                            <div key={card._id} className="relative transition duration-300 bg-white rounded-lg shadow-xl hover:shadow-2xl">
+                                <LiveProvider code={card.code}>
+                                    <div className="min-h-[50vh] bg-gradient-to-r from-blue-300 to-blue-200 relative rounded-t-lg">
+                                        <div className="absolute inset-0 overflow-auto text-neutral-950 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-blue-200">
+                                            <LivePreview />
+                                        </div>
                                     </div>
-                                </div>
-                            </LiveProvider>
-                            <div className="flex items-center justify-center px-2 mb-2">
-                                <div className="flex items-center space-x-3">
-                                    <div>
-                                        <Link href={`/component/${card._id}`}>
-                                            <button
-                                                className="px-4 py-2 text-white transition-transform duration-300 ease-in-out rounded-full bg-gradient-to-r from-blue-400 to-indigo-500 hover:from-indigo-500 hover:to-purple-500 hover:shadow-2xl focus:outline-none focus:ring focus:border-blue-300 transform-style-preserve-3d text"
-                                            >
-                                                <div className="flex items-center space-x-2">
-                                                    <FaCode className="text-xl md:text-3xl" />
-                                                    <span className="text-lg">Explore</span>
-                                                </div>
-                                            </button>
-                                        </Link>
-                                    </div>
+                                </LiveProvider>
+                                <div className="flex items-center justify-center p-4">
+                                    <Link href={`/component/${card._id}`}>
+                                        <button className="px-4 py-2 text-white rounded-full bg-gradient-to-r from-blue-400 to-indigo-500 hover:from-indigo-500 hover:to-purple-500 focus:outline-none focus:ring focus:border-blue-300">
+                                            <div className="flex items-center space-x-2">
+                                                <FaCode className="text-xl" />
+                                                <span className="text-lg">Explore</span>
+                                            </div>
+                                        </button>
+                                    </Link>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </Container>
     );
